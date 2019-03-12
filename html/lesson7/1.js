491c1a4100a1adddb3e410164e2cdcbc1efd7ed9
huangyisan <ahuangyisan@live.cn>
2019-03-12 22:00:14 +0800

    update

---
+++ html/lesson7/1.js
@@ -0,0 +1,5 @@
+var x = 'aa11.7aa';
+console.log(typeof Number(x));
+console.log(parseInt(x));
+
+console.log(typeof(typeof undefined));
---
+++ html/object/ob.js
@@ -0,0 +1,11 @@
+var myFunc = {
+    name : "yisan",
+    age : 28,
+    sex: "male",
+    smoke : function () {
+        console.log("i am smoking")
+    },
+    drink : function () {
+        console.log('i am drinking')
+    }
+}
