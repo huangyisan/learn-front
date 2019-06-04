039ff9b8780e0a9a687ffce5d343a7b13a90b3f1
huangyisan <ahuangyisan@live.cn>
2019-06-04 10:22:16 +0800

    add learn vue

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 > https://ke.qq.com/webcourse/index.html#cid=231570&term_id=100273162&taid=1441309420390546&vid=q1419f03abt
+
+## Vue
+
+> https://www.imooc.com/video/16975
--- html/object/ob.js
+++ html/object/ob.js
@@ -2,10 +2,16 @@ var myFunc = {
     name : "yisan",
     age : 28,
     sex: "male",
+    health: 100,
     smoke : function () {
-        console.log("i am smoking")
+        console.log("i am smoking");
+        this.health --;
     },
     drink : function () {
-        console.log('i am drinking')
+        console.log('i am drinking');
+        this.health ++;
     }
-}
+};
+
+myFunc.house = "bieshu";
+delete myFunc.house
