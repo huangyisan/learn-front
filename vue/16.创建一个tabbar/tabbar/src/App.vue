b716a288d5f3dd4034ecb72708c0ba889eb5b3b7
huangyisan <ahuangyisan@live.cn>
2020-02-15 00:14:42 +0800

    update vue

--- vue/16.创建一个tabbar/tabbar/src/App.vue
+++ vue/16.创建一个tabbar/tabbar/src/App.vue
@@ -1,23 +1,46 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <div>
+    <tab-bar>
+      <tab-bar-item>
+        <!--插槽上不要写样式属性,因为slot会被替换,写了样式属性不会生效-->
+        <img slot="item-icon" src="./assets/img/tabbar/home.png" alt="">
+        <img slot="item-icon-active" src="./assets/img/tabbar/home-active.png" alt="">
+        <div slot="item-text">首页</div>
+      </tab-bar-item>
+      <tab-bar-item>
+        <img slot="item-icon" src="./assets/img/tabbar/category.png" alt="">
+        <img slot="item-icon-active" src="./assets/img/tabbar/category-active.png" alt="">
+        <div slot="item-text">分类</div>
+      </tab-bar-item>
+      <tab-bar-item>
+        <img slot="item-icon" src="./assets/img/tabbar/cart.png" alt="">
+        <img slot="item-icon-active" src="./assets/img/tabbar/cart-active.png" alt="">
+        <div slot="item-text">购物车</div>
+      </tab-bar-item>
+      <tab-bar-item>
+        <img slot="item-icon" src="./assets/img/tabbar/profile.png" alt="">
+        <img slot="item-icon-active" src="./assets/img/tabbar/profile-active.png" alt="">
+        <div slot="item-text">我的</div>
+      </tab-bar-item>
+    </tab-bar>
+
+
   </div>
 </template>
 
 <script>
+  import TabBar from './components/tabbar/TabBar.vue'
+  import TabBarItem from './components/tabbar/TabBarItem.vue'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    TabBar: TabBar,
+    TabBarItem: TabBarItem
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ vue/16.创建一个tabbar/tabbar/src/components/tabbar/TabBarItem.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="tab-bar-item">
+    <!--配置具名插槽,用来放图片和名称-->
+    <div v-if="!isActive"><slot name="item-icon"></slot></div>
+    <div v-else-if="isActive"><slot name="item-icon-active"></slot></div>
+    <div :class="{active: isActive}"><slot name="item-text"></slot></div>
+
+
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "TbaBarItem",
+    data() {
+      return {
+        isActive: true
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .tab-bar-item {
+    /*均分*/
+    flex: 1;
+    text-align: center;
+    height: 49px;
+    font-size: 14px;
+    /*line-height: 49px;*/
+
+  }
+
+  .tab-bar-item img {
+    margin-top: 3px;
+    width: 20px;
+    height: 20px;
+    /* 去掉图片下面的空间 */
+    vertical-align: middle;
+    margin-bottom: 2px;
+  }
+
+  .active {
+    color: red;
+  }
+</style>
