046c076112cb55f6646465b3b6315fca6b4145f8
huangyisan <ahuangyisan@live.cn>
2020-02-15 01:37:05 +0800

    update vue

--- vue/16.创建一个tabbar/tabbar/src/App.vue
+++ vue/16.创建一个tabbar/tabbar/src/App.vue
@@ -1,28 +1,9 @@
 <template>
-  <div>
-    <tab-bar>
-      <tab-bar-item>
-        <!--插槽上不要写样式属性,因为slot会被替换,写了样式属性不会生效-->
-        <img slot="item-icon" src="./assets/img/tabbar/home.png" alt="">
-        <img slot="item-icon-active" src="./assets/img/tabbar/home-active.png" alt="">
-        <div slot="item-text">首页</div>
-      </tab-bar-item>
-      <tab-bar-item>
-        <img slot="item-icon" src="./assets/img/tabbar/category.png" alt="">
-        <img slot="item-icon-active" src="./assets/img/tabbar/category-active.png" alt="">
-        <div slot="item-text">分类</div>
-      </tab-bar-item>
-      <tab-bar-item>
-        <img slot="item-icon" src="./assets/img/tabbar/cart.png" alt="">
-        <img slot="item-icon-active" src="./assets/img/tabbar/cart-active.png" alt="">
-        <div slot="item-text">购物车</div>
-      </tab-bar-item>
-      <tab-bar-item>
-        <img slot="item-icon" src="./assets/img/tabbar/profile.png" alt="">
-        <img slot="item-icon-active" src="./assets/img/tabbar/profile-active.png" alt="">
-        <div slot="item-text">我的</div>
-      </tab-bar-item>
-    </tab-bar>
+  <div id="App">
+    <router-view></router-view>
+    <main-tab-bar></main-tab-bar>
+
+
 
 
   </div>
@@ -31,14 +12,16 @@
 <script>
   import TabBar from './components/tabbar/TabBar.vue'
   import TabBarItem from './components/tabbar/TabBarItem.vue'
+  import MainTabBar from './components/MainTabBar.vue'
 
-export default {
-  name: 'App',
-  components: {
-    TabBar: TabBar,
-    TabBarItem: TabBarItem
+  export default {
+    name: 'App',
+    components: {
+      TabBar: TabBar,
+      TabBarItem: TabBarItem,
+      MainTabBar:MainTabBar,
+    }
   }
-}
 </script>
 
 <style>
