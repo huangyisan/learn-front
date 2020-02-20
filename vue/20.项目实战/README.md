2f1df2ffd638158eb8c7c3593cdae5f8e21b84f5
huangyisan <ahuangyisan@live.cn>
2020-02-20 16:20:54 +0800

    update vue\

---
+++ vue/20.项目实战/README.md
@@ -0,0 +1,4 @@
+## 步骤
+1. 划分好文件夹
+2. 下载用来统一样式的css文件, normalize.css, https://necolas.github.io/normalize.css/latest/normalize.css
+3. 建立属于自己的统一样式文件, base.css
---
+++ vue/20.项目实战/supermall/src/views/home/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div><h2>首页1</h2></div>
+
+</template>
+
+<script>
+  export default {
+    name: "Home"
+  }
+</script>
+
+<style scoped>
+  div {
+    background-color: #f00;
+  }
+</style>
