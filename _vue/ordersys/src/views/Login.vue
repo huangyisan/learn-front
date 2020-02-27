8ec695e3996a6aed72ca3a5aa4219997dff13e8b
huangyisan <anonymousyisan@gmail.com>
2020-02-27 11:24:08 +0800

    update

---
+++ _vue/ordersys/src/views/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="login">
+    <el-form>
+      <el-form-item label="用户名"></el-form-item>
+      <el-form-item label="密码"></el-form-item>
+    </el-form>
+  </div>
+    
+</template>
+
+<script>
+    export default {
+        name: "Login"
+    }
+</script>
+
+<style scoped>
+
+</style>
