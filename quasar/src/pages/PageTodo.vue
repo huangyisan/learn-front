35fcce2f59dc9d54deeb535d6002ad697f2493fd
huangyisan <ahuangyisan@live.cn>
2020-08-21 22:36:03 +0800

    todo list style

--- quasar/src/pages/PageTodo.vue
+++ quasar/src/pages/PageTodo.vue
@@ -1,16 +1,34 @@
 <template>
   <q-page class="q-pa-md">
-    <q-list bordered>
+    <q-list separator bordered>
       <q-item v-for="(item, index) in tasks" 
         :key="index" 
         @click="item.complate = !item.complate" 
         clickable 
-        v-ripple>
+        v-ripple
+        :class="!item.complate ? 'bg-orange-1' : 'bg-green-1'">
         <q-item-section side top>
           <q-checkbox v-model="item.complate" />
         </q-item-section>
         <q-item-section>
-          <q-item-label>{{item.name}}</q-item-label>
+          <!-- 自定义一个text-strikethrough的class, 然后去App.vue里面全局定义 -->
+          <q-item-label :class="{ 'text-strikethrough' : item.complate }">{{item.name}}</q-item-label>
+        </q-item-section>
+        <q-item-section side>
+          <div class="row">
+            <q-icon name="event" color="" size="18px" class="q-mr-xs" />
+            <div class="column">
+              <q-item-label caption class="row justify-end">
+                {{item.dueDate}}
+              </q-item-label>
+              <q-item-label caption class="row justify-end">
+                <small>
+                  {{item.dueTime}}
+                </small>
+              </q-item-label>
+            </div>
+          </div>
+
         </q-item-section>
       </q-item>
     </q-list>
@@ -25,17 +43,23 @@ export default {
         {
           id: 1,
           name: "go to shop",
-          complate: false
+          complate: false,
+          dueDate: '2020/01/01',
+          dueTime: '22:22:33'
         },
         {
           id: 2,
           name: "go to home",
-          complate: false
+          complate: false,
+          dueDate: '2020/01/02',
+          dueTime: '12:22:33'
         },
         {
           id: 3,
           name: "go to school",
-          complate: true
+          complate: true,
+          dueDate: '2020/01/03',
+          dueTime: '02:22:33'
         }
       ]
     }
