4a9c5bfeac7ea47591a6165fdea4e556da7055df
huangyisan <ahuangyisan@live.cn>
2020-08-23 12:05:30 +0800

    添加日期 时间输入

--- quasar/src/components/Modals/AddTask.vue
+++ quasar/src/components/Modals/AddTask.vue
@@ -9,16 +9,34 @@
     </q-card-section>
 
     <q-card-section class="q-pt-none">
-      <q-input outlined v-model="taskToSubmit.name" label="Task Name" />
-      <q-input outlined v-model="taskToSubmit.dueDate">
-        <template v-slot:append>
-          <q-icon name="event" class="cursor-pointer">
-            <q-popup-proxy ref="qDateProxy" transition-show="scale" transition-hide="scale">
-              <q-date v-model="taskToSubmit.dueDate" @input="() => $refs.qDateProxy.hide()" />
-            </q-popup-proxy>
-          </q-icon>
-        </template>
-      </q-input>
+      <div class="row q-mb-sm">
+        <q-input outlined v-model="taskToSubmit.name" label="Task Name" class="col" />
+      </div>
+
+      <div class="row q-mb-sm">
+        <q-input outlined v-model="taskToSubmit.dueDate" label="Due Date">
+          <template v-slot:append>
+            <q-icon name="event" class="cursor-pointer">
+              <q-popup-proxy ref="qDateProxy" transition-show="scale" transition-hide="scale">
+                <q-date v-model="taskToSubmit.dueDate" @input="() => $refs.qDateProxy.hide()" />
+              </q-popup-proxy>
+            </q-icon>
+          </template>
+        </q-input>
+      </div>
+
+      <div class="row q-mb-sm">
+        <q-input outlined v-model="taskToSubmit.dueTime" label="Due Time">
+          <template v-slot:append>
+            <q-icon name="access_time" class="cursor-pointer">
+              <q-popup-proxy transition-show="scale" transition-hide="scale">
+                <q-time v-model="taskToSubmit.dueTime" />
+              </q-popup-proxy>
+            </q-icon>
+          </template>
+        </q-input>
+      </div>
+      
     </q-card-section>
 
     <q-card-actions align="right">
