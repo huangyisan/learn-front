ec9e9fe1d2ce58bb563134747f684e02e175caf4
= <anonymousyisan@gmail.com>
2020-09-03 08:12:42 +0800

    searchbar

--- quasar/src/components/Tasks/Tools/Search.vue
+++ quasar/src/components/Tasks/Tools/Search.vue
@@ -13,6 +13,7 @@
 import { mapState } from 'vuex'
 export default {
   computed: {
+    // 这里使用mapState而不使用mapGetter是因为不需要做一些处理，该数据直接从State中获取即可。
     ...mapState('tasks', ['search']),
     searchField() {
       return this.search
