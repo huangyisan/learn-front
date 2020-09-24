f2b1de304e10c5de4a5c7ab71b51662a9f1c96a7
huangyisan <ahuangyisan@live.cn>
2020-09-24 15:29:49 +0800

    clean search bar when click esc

--- quasar/src/components/Tasks/Tools/Search.vue
+++ quasar/src/components/Tasks/Tools/Search.vue
@@ -1,5 +1,12 @@
 <template>
-  <q-input outlined v-select-all v-model="searchField" label="Search" class="col">
+<!-- 绑定了点击esc,清除搜索栏 -->
+  <q-input 
+    outlined 
+    v-select-all 
+    v-model="searchField" 
+    @keyup.esc="searchField = ''"
+    label="Search" 
+    class="col">
 
     <template v-slot:append>
       <q-icon v-if="searchField !== ''" name="close" @click="searchField = ''" class="cursor-pointer" />
