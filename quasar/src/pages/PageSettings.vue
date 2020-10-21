6e9d9032aa8579339b74b32ed464964a655386a1
huangyisan <anonymousyisan@gmail.com>
2020-10-21 20:31:50 +0800

    添加settings功能

--- quasar/src/pages/PageSettings.vue
+++ quasar/src/pages/PageSettings.vue
@@ -1,11 +1,29 @@
 <template>
   <q-page padding>
-    <p>Page settings</p>
+    <q-list bordered padding>
+      <q-item-label header>Settings</q-item-label>
+        <q-item tag="label" v-ripple>
+          <q-item-section>
+            <q-item-label>Show 12 hour time format</q-item-label>
+          </q-item-section>
+          <q-item-section side>
+            <q-toggle 
+              color="blue" 
+              v-model="show12HourTimeFormat" 
+              />
+          </q-item-section>
+        </q-item>
+        </q-list>
   </q-page>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      show12HourTimeFormat: false
+    }
+  }
 }
 </script>
  
