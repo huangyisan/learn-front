6dabd74344e6492f64e1c1616fce5d8915d54c99
= <anonymousyisan@gmail.com>
2020-10-22 15:25:16 +0800

    store localstorage

--- quasar/src/store/store-settings.js
+++ quasar/src/store/store-settings.js
@@ -1,3 +1,5 @@
+import { LocalStorage } from 'quasar'
+
 const state = {
   settings: {
     show12HourTimeFormat: true
@@ -11,8 +13,16 @@ const mutations = {
 }
 
 const actions = {
-  setShow12HourTimeFormat({ commit }, value) {
+  setShow12HourTimeFormat({ commit, dispatch }, value) {
     commit('setShow12HourTimeFormat', value)
+    // 同时异步调用saveSettings函数
+    dispatch('saveSettings')
+  },
+
+  // 将state内容存入用户浏览器localStorage中
+  saveSettings({ state }){
+    LocalStorage.set('settings', state.settings)
+    // localStorage.set('settings', state.settings)
   }
 }
 
