353ab0f368d026e7b0e9a0e3c597f994b44d04fc
= <anonymousyisan@gmail.com>
2020-10-24 15:47:59 +0800

    register form

--- quasar/src/components/Auth/Register.vue
+++ quasar/src/components/Auth/Register.vue
@@ -1,15 +1,58 @@
 <template>
-  <q-banner class="bg-grey-3">
-    <template v-slot:avatar>
-      <q-icon name="account_circle" color="primary" />
-    </template>
-    Register to access your todos anywhere
+<form>
+  <div class="row q-mb-md">
+    <q-banner class="bg-grey-3 col">
+      <template v-slot:avatar>
+        <q-icon name="account_circle" color="primary" />
+      </template>
+      Register to access your todos anywhere
+    </q-banner>
+  </div>
 
-  </q-banner>
+  <div class="row q-mb-md">
+    <q-input
+      class="col"
+      outlined 
+      v-model="formData.email" 
+      label="Email" 
+      stack-label 
+      />
+  </div>
+
+  <div class="row q-mb-md">
+    <q-input
+      class="col"
+      outlined 
+      v-model="formData.password" 
+      label="Password" 
+      stack-label
+      type="password"
+      />
+  </div>
+
+  <div class="row">
+    <!-- 靠右 -->
+    <q-space></q-space>
+    <q-btn 
+      color="primary"
+      label="Register"
+    />
+  </div>
+
+</form>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      formData: {
+        email: "",
+        password: ""
+      }
+    }
+  }
+};
 </script>
 
 <style>
