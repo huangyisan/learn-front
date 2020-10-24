7062e1750c5157197c28512aca9fb831437e2d8d
= <anonymousyisan@gmail.com>
2020-10-24 17:34:42 +0800

    login and register

--- quasar/src/components/Auth/LoginRegister.vue
+++ quasar/src/components/Auth/LoginRegister.vue
@@ -12,6 +12,7 @@
 
   <div class="row q-mb-md">
     <q-input
+      ref="email"
       class="col"
       outlined 
       v-model="formData.email" 
@@ -24,10 +25,11 @@
 
   <div class="row q-mb-md">
     <q-input
+      ref="password"
       class="col"
       outlined 
-      v-model="formData.password" 
-      label="Password" 
+      v-model="formData.password"
+      label="Password"
       stack-label
       type="password"
       :rules="[ val => val.length >= 6 || 'Please use at least 6 characters']"
@@ -51,6 +53,8 @@
 
 <script>
 export default {
+  // 父级传入tab
+  props: ['tab'],
   data() {
     return {
       formData: {
@@ -59,9 +63,19 @@ export default {
       }
     }
   },
-  methods:{
+  methods: {
     submitForm() {
-      console.log('submit form')
+      this.$refs.email.validate()
+      this.$refs.password.validate()
+      if (!this.$refs.email.hasError && !this.$refs.email.hasError) {
+        // 根据父级传入的tab判断是注册还是登陆
+        if (this.tab == 'register') {
+          console.log("register user")
+        } else {
+          console.log("login user")
+
+        }
+      }
     },
     isValidEmailAddress(email) {
       const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
