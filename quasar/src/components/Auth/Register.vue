fcf5b9934de1e040c64ea5203f0fe852698d124f
= <anonymousyisan@gmail.com>
2020-10-24 15:21:50 +0800

    register组件

---
+++ quasar/src/components/Auth/Register.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-banner class="bg-grey-3">
+    <template v-slot:avatar>
+      <q-icon name="account_circle" color="primary" />
+    </template>
+    Register to access your todos anywhere
+
+  </q-banner>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
