72a2a8d194ef9d4ae309aedcdfee39f3d7522abc
huangyisan <anonymousyisan@gmail.com>
2020-11-04 21:00:43 +0800

    listen on child_removed

--- quasar/src/store/store-tasks.js
+++ quasar/src/store/store-tasks.js
@@ -122,6 +122,23 @@ const actions = {
       // 将payload作用在名称为updateTask的mutation上
       commit('updateTask', payload)
     })
+    
+    // child remove
+    userTasks.on('child_removed', snapshot => {
+      // 获取child节点的value
+      let task = snapshot.val()
+      
+      //拼装payload用于mutation提交
+      let payload = {
+        id: snapshot.key,
+        task: task,
+
+      }
+
+      // 将payload作用在名称为deleteTask的mutation上
+      commit('deleteTask', payload)
+    })
+    
 
 
   }
